508c3b1531d6aa29df0599271d49d12dd3d7db74
golrice <791554926@qq.com>
2023-08-23 17:25:36 +0800

    fix timer

--- Timer/Timer.hpp
+++ Timer/Timer.hpp
@@ -2,23 +2,25 @@
 
 #include <chrono>
 #include <iostream>
+#include <thread>
 
 class Timer
 {
 public:
     Timer()
     {
-        _start = std::chrono::high_resolution_clock::now();
+        _start = std::chrono::steady_clock::now();
     }
     ~Timer()
     {
-        _end = std::chrono::high_resolution_clock::now();
-        _duration = (_end - _start);
+        _end = std::chrono::steady_clock::now();
+        _duration = std::chrono::duration_cast<std::chrono::microseconds>(_end - _start);
 
-        std::cout << "Time elapsed: " << _duration.count() * 1000.0 << " ns" << std::endl;
+        std::cout << "Time elapsed: " << _duration.count() << " ns" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
 public:
-    std::chrono::time_point<std::chrono::high_resolution_clock> _start, _end;
-    std::chrono::duration<double> _duration;
+    std::chrono::time_point<std::chrono::steady_clock> _start, _end;
+    std::chrono::duration<double, std::micro> _duration;
 };
